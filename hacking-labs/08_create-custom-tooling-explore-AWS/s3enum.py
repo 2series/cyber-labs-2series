b0717644baabed097dd5a177efa3553d80a32121
Rihad Variawa <rihadv021@gmail.com>
2025-01-20 01:57:30 +0200

    Update s3enum.py to use AWS URL instead of bucket URL

--- hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
+++ hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
@@ -1,5 +1,5 @@
 # import requests
 
-# bucket_url = "https://s3.amazonaws.com:443/dev.challenge.com?list-type=2&prefix=&delimiter=%2F&encoding-type=url"
-# response = requests.get(bucket_url)
+# aws_url = "https://s3.amazonaws.com:443/dev.challenge.com?list-type=2&prefix=&delimiter=%2F&encoding-type=url"
+# response = requests.get(aws_url)
 # print(response)

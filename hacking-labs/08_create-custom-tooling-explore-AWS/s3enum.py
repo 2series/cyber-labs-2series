a9130807ca4b390a9a1f5b96035793eebc328fdb
Rihad Variawa <rihadv021@gmail.com>
2025-01-20 01:59:40 +0200

    Removed commented out import and code, added import and uncommented code

--- hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
+++ hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
@@ -1,5 +1,5 @@
-# import requests
+import requests
 
-# aws_url = "https://s3.amazonaws.com:443/dev.challenge.com?list-type=2&prefix=&delimiter=%2F&encoding-type=url"
-# response = requests.get(aws_url)
-# print(response)
+url = "https://s3.amazonaws.com:443/dev.challenge.com?list-type=2&prefix=&delimiter=%2F&encoding-type=url"
+response = requests.get(url)
+print(response)

4ab177356bc167fe5f69c977fc93a03a2a4e6e9b
Rihad Variawa <rihadv021@gmail.com>
2025-01-20 02:08:33 +0200

    File update

---
+++ hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
@@ -0,0 +1,6 @@
+# import requests
+
+# aws_url = "https://s3.amazonaws.com:443/dev.challenge.com?list-type=2&prefix=&delimiter=%2F&encoding-type=url"
+# response = requests.get(aws_url)
+# print(response)
+

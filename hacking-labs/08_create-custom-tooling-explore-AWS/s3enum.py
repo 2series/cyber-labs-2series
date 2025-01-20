e078b69f5da908e1a746b23129eec1f3138ea5b0
Rihad Variawa <rihadv021@gmail.com>
2025-01-20 02:26:36 +0200

    Added AWS S3 enumeration code with commented out original import and URL

--- hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
+++ hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
@@ -1,8 +1,9 @@
-# import requests
+import requests
 
-# aws_url = (
-#     "https://s3.amazonaws.com:443/dev.challenge.com?"
-#     "list-type=2&prefix=&delimiter=%2F&encoding-type=url"
-# )
-# response = requests.get(aws_url)
-# print(response)
+aws_url = (
+    "https://s3.amazonaws.com:443/dev.challenge.com?"
+    "list-type=2&prefix=&delimiter=%2F&encoding-type=url"
+)
+response = requests.get(aws_url)
+print(response)
+

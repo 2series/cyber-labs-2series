8cbd83e7fe03c1544d5f41ec2391e8976e6943fc
Rihad Variawa <rihadv021@gmail.com>
2025-01-20 02:35:45 +0200

    Update s3enum.py: replace 'response' with 'resp' in print statement

--- hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
+++ hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
@@ -4,5 +4,5 @@ aws_url = (
     "https://s3.amazonaws.com:443/dev.challenge.com?"
     "list-type=2&prefix=&delimiter=%2F&encoding-type=url"
 )
-response = requests.get(aws_url)
-print(response)
+resp = requests.get(aws_url)
+print(resp)

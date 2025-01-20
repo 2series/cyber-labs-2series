0fae3543ec676eae6fc4f7c3cfb956d695767cf8
Rihad Variawa <rihadv021@gmail.com>
2025-01-20 02:17:49 +0200

    Added AWS URL string to s3enum.py and commented out related code

--- hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
+++ hacking-labs/08_create-custom-tooling-explore-AWS/s3enum.py
@@ -1,6 +1,8 @@
 # import requests
 
-# aws_url = "https://s3.amazonaws.com:443/dev.challenge.com?list-type=2&prefix=&delimiter=%2F&encoding-type=url"
+# aws_url = (
+#     "https://s3.amazonaws.com:443/dev.challenge.com?"
+#     "list-type=2&prefix=&delimiter=%2F&encoding-type=url"
+# )
 # response = requests.get(aws_url)
 # print(response)
-

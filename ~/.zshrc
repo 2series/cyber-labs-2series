7498a3bcf963b2db82a7b7c32c13a28ff1356ce1
Rihad Variawa <rihadv021@gmail.com>
2025-01-28 13:21:55 +0200

    Removed .DS_Store from .gitignore, deleted clear_history.sh and its reference in .zshrc

--- ~/.zshrc
+++
@@ -1,2 +0,0 @@
-# Clear zsh history on terminal start
-~/Documents/cyber-labs-2series/clear_history.sh
--- ~/clear_history.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# Clear the zsh history
-history -c
-# Optionally, you can also remove the history file
-rm -f ~/.zsh_history
-# Create a new empty history file
-touch ~/.zsh_history
